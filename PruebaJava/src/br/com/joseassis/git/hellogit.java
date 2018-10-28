9d467acd603b4c42a0821571335d6b5cd676a451
gsgaitan <44374179+gsgaitan@users.noreply.github.com>
2018-10-28 18:37:21 -0500

    Add files via upload

---
+++ PruebaJava/src/br/com/joseassis/git/hellogit.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package br.com.joseassis.git;
+
+/**
+ * @author ggaitan
+ *
+ */
+public class hellogit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
